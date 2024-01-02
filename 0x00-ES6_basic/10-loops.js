8c714e79a6a16422fa2b9f73c7928b64fe6c62a1
Victor Langat <provicml@gmail.com>
2024-01-02 18:47:29 +0300

    task added: for of loop

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,14 @@
+//  for of does not give access to idx of elems: disable es6
+/* eslint no-param-reassign: ["error",
+{ "props": true, "ignorePropertyModificationsFor": ["array"] }] */
+export default function appendToEachArrayValue(array, appendString) {
+  // let i = 0;
+  // (const [i, el] of myArr.entries())
+  // for (const idx of array.entries()) {
+  for (const [i, idx] of array.entries()) {
+    array[i] = appendString + idx;
+    // i += 1;
+  }
+
+  return array;
+}
