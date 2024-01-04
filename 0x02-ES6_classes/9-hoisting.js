87a4279d63a13441f363d11081e62f4cdf9a241e
Victor Langat <provicml@gmail.com>
2024-01-04 17:50:48 +0300

    codefix: correct output test formatting

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -32,6 +32,7 @@ export class StudentHolberton {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
+
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
