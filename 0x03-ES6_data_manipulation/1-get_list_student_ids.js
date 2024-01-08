32d1924d6e1a71ee2ab5e44b85006ccfad7d04b3
Victor Langat <provicml@gmail.com>
2024-01-08 23:06:08 +0300

    fix task 1- use basic map

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,6 @@ export default (array) => {
   if (!Array.isArray(array)) {
     return [];
   }
-  return Array.prototype.map.call(array, ({ id }) => id);
+  // return Array.prototype.map.call(array, ({ id }) => id);
+  return array.map((elems) => elems.id);
 };
