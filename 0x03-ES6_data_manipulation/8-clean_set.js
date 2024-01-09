4ed50810a3d7ef5e8468ea029d24fa680e90e393
Victor Langat <provicml@gmail.com>
2024-01-09 09:00:03 +0300

    test clean set fix: correct output v2

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,7 +5,7 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
   for (let i = 0; i < arr.length; i += 1) {
     const elem = arr[i];
-    if (elem.startsWith(startString)) {
+    if (elem.startsWith(startString) && elem && elem.length > 0) {
       if (i !== arr.length - 1) separator = '-';
       str += elem.slice(startString.length) + separator;
     }
