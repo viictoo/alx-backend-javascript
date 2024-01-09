d2655f94e52aa756f8fbc1500b65764a1b8e6d11
Victor Langat <provicml@gmail.com>
2024-01-09 09:51:51 +0300

    test clean set fix: correct output v5- test non string input

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,22 +1,31 @@
-// export default function cleanSet(set, startString) {
-//   const str = [];
-//   const separator = '-';
-//   const arr = Array.from(set);
-//   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
-//   for (let i = 0; i < arr.length; i += 1) {
-//     const elem = arr[i];
-//     if (elem.startsWith(startString) && typeof elem === 'string' && elem.length > 0) {
-//       str.push(elem.slice(startString.length));
-//     }
-//   }
-//   return str.join(separator);
-// }
-// complete solution
 export default function cleanSet(set, startString) {
+  const str = [];
+  const separator = '-';
+  const arr = Array.from(set);
   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
-  // if (startString === '') return '';
-  return [...set]
-    .filter((el) => typeof el === 'string' && el.toLowerCase().startsWith(startString.toLowerCase()))
-    .map((el) => el.slice(startString.length))
-    .join('-');
+  for (let i = 0; i < arr.length; i += 1) {
+    const elem = arr[i];
+    if (typeof elem === 'string' && elem.length > 0 && elem.startsWith(startString)) {
+      str.push(elem.slice(startString.length));
+    }
+  }
+  return str.join(separator);
 }
+// complete solution using spread
+// export default function cleanSet(set, startString) {
+//   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
+//   return [...set]
+//     .filter((el) => typeof el === 'string' && el.toLowerCase()
+// .startsWith(startString.toLowerCase()))
+//     .map((el) => el.slice(startString.length))
+//     .join('-');
+// }
+
+// bare minimum valid answer
+// export default function cleanSet(set, startString) {
+//   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
+//   return [...set]
+//     .filter((el) => el.startsWith(startString))
+//     .map((el) => el.slice(startString.length))
+//     .join('-');
+// }
