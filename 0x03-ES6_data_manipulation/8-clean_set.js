4eb443b3dcb1ea54f3fb06024d95e34c6014eafa
Victor Langat <provicml@gmail.com>
2024-01-09 10:01:45 +0300

    test clean set fix: correct output v5- test non string input: fixed✅

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,21 +5,13 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
   for (let i = 0; i < arr.length; i += 1) {
     const elem = arr[i];
+    // order of conditions matters: check startswith last
     if (typeof elem === 'string' && elem.length > 0 && elem.startsWith(startString)) {
       str.push(elem.slice(startString.length));
     }
   }
   return str.join(separator);
 }
-// complete solution using spread
-// export default function cleanSet(set, startString) {
-//   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
-//   return [...set]
-//     .filter((el) => typeof el === 'string' && el.toLowerCase()
-// .startsWith(startString.toLowerCase()))
-//     .map((el) => el.slice(startString.length))
-//     .join('-');
-// }
 
 // bare minimum valid answer
 // export default function cleanSet(set, startString) {
