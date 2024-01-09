2266f4c832559a98b0d36f2873ce4a3e2912f163
Victor Langat <provicml@gmail.com>
2024-01-09 09:33:19 +0300

    test clean set fix: correct output v5- test test

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,21 @@
+// export default function cleanSet(set, startString) {
+//   const str = [];
+//   const separator = '-';
+//   const arr = Array.from(set);
+//   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
+//   for (let i = 0; i < arr.length; i += 1) {
+//     const elem = arr[i];
+//     if (elem.startsWith(startString) && typeof elem === 'string' && elem.length > 0) {
+//       str.push(elem.slice(startString.length));
+//     }
+//   }
+//   return str.join(separator);
+// }
 export default function cleanSet(set, startString) {
-  const str = [];
-  const separator = '-';
-  const arr = Array.from(set);
   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
-  for (let i = 0; i < arr.length; i += 1) {
-    const elem = arr[i];
-    if (elem.startsWith(startString) && typeof elem === 'string' && elem.length > 0) {
-      str.push(elem.slice(startString.length));
-    }
-  }
-  return str.join(separator);
+  if (startString === '') return '';
+  return [...set]
+    .filter((el) => typeof el === 'string' && el.toLowerCase().startsWith(startString.toLowerCase()))
+    .map((el) => el.slice(startString.length))
+    .join('-');
 }
