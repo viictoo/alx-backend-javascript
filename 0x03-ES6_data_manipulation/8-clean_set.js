6138e3c22eed9ffe751c4b47e0004f759b1053d4
Victor Langat <provicml@gmail.com>
2024-01-09 09:04:49 +0300

    test clean set fix: correct output v3-use join

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,13 @@
 export default function cleanSet(set, startString) {
-  let str = '';
-  let separator = '';
+  const str = [];
+  const separator = '-';
   const arr = Array.from(set);
   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
   for (let i = 0; i < arr.length; i += 1) {
     const elem = arr[i];
     if (elem.startsWith(startString) && elem && elem.length > 0) {
-      if (i !== arr.length - 1) separator = '-';
-      str += elem.slice(startString.length) + separator;
+      str.push(elem.slice(startString.length));
     }
   }
-  return str.endsWith('-') ? str.slice(0, -1) : str;
+  return str.join(separator);
 }
