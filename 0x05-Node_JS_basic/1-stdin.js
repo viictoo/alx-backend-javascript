57b0c625d80b30c5303d3337f27f17949464a98b
Victor Langat <provicml@gmail.com>
2024-02-26 21:33:17 +0300

    add code: read file sync

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  let chunk;
+  if ((chunk = process.stdin.read()) !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
