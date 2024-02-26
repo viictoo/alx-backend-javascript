17c0637804cf47b6b528bcfb31e720f601124cc5
Victor Langat <provicml@gmail.com>
2024-02-26 21:33:42 +0300

    add code: read file async

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,36 @@
-#!/usr/bin/node
+// get an present data from a csv file using a  hash mapp
+const fs = require('fs');
+const readline = require('readline');
+
+function countStudents (path) {
+  let studentNum = 0;
+  const subjects = new Map();
+
+  const stream = fs.createReadStream(path);
+  const reader = readline.createInterface({
+    input: stream,
+    crlfDelay: Infinity
+  });
+
+  reader.on('line', (line) => {
+    const columns = line.split(',');
+    const subject = columns[columns.length - 1];
+    if (subject !== 'field') {
+      studentNum++;
+      if (!subjects.has(subject)) {
+        subjects.set(subject, []);
+      }
+      subjects.get(subject).push(columns[0]);
+    }
+  });
+  reader.on('error', () => { throw new Error('Cannot load the database'); });
+
+  reader.on('close', () => {
+    console.log(`Number of students: ${studentNum}`);
+    subjects.forEach((students, subject) => {
+      console.log(`Number of students in ${subject}: ${students.length}. List: ${students.join(', ')}`);
+    });
+  });
+}
+
+module.exports = countStudents;
