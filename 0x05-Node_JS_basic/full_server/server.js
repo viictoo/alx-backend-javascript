16cb68728ab97e10151397d4b49a9417ec5ac97d
Victor Langat <provicml@gmail.com>
2024-02-27 23:55:46 +0300

    code added: server

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+import express from 'express';
+
+const app = express();
+const indexRouter = require('./routes/index');
+const port = 1245;
+
+app.listen(port);
+app.use('/', indexRouter);
+app.use('/students', indexRouter);
+app.use('/students/:major', indexRouter);
+
+export default app;
