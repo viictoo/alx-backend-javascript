42d2045df9f9cd2ab9551afa32fdf057708156f9
Victor Langat <provicml@gmail.com>
2024-02-28 00:13:38 +0300

    semistandard

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  let chunk;
-  if ((chunk = process.stdin.read()) !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-
