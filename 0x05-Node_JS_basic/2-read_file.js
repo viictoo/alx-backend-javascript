67ad740b44ea46e23bf33a9c17b18f36f5856497
Victor Langat <provicml@gmail.com>
2024-02-28 00:35:14 +0300

    requirement permission

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,30 +1,35 @@
 const fs = require('fs');
+const readline = require('readline');
 
-function countStudents(path) {
-  if (!fs.existsSync(path)) {
-    throw Error('Cannot load the database');
-  }
-  // block other parallel process
-  // and do the current file reading process
-  const data = fs.readFileSync(path, 'utf8');
-  const students = data.split('\n')
-    .map((student) => student.split(','))
-    .filter((student) => student.length === 4 && student[0] !== 'firstname')
-    .map((student) => ({
-      firstName: student[0],
-      lastName: student[1],
-      age: student[2],
-      field: student[3],
-    }));
-  const csStudents = students
-    .filter((student) => student.field === 'CS')
-    .map((student) => student.firstName);
-  const sweStudents = students
-    .filter((student) => student.field === 'SWE')
-    .map((student) => student.firstName);
-  console.log(`Number of students: ${students.length}`);
-  console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
-  console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+function countStudents (path) {
+  let studentNum = 0;
+  const subjects = new Map();
+
+  const stream = fs.createReadStream(path);
+  const reader = readline.createInterface({
+    input: stream,
+    crlfDelay: Infinity
+  });
+
+  reader.on('line', (line) => {
+    const columns = line.split(',');
+    const subject = columns[columns.length - 1];
+    if (subject !== 'field') {
+      studentNum++;
+      if (!subjects.has(subject)) {
+        subjects.set(subject, []);
+      }
+      subjects.get(subject).push(columns[0]);
+    }
+  });
+  reader.on('error', () => { throw new Error('Cannot load the database'); });
+
+  reader.on('close', () => {
+    console.log(`Number of students: ${studentNum}`);
+    subjects.forEach((students, subject) => {
+      console.log(`Number of students in ${subject}: ${students.length}. List: ${students.join(', ')}`);
+    });
+  });
 }
 
 module.exports = countStudents;
