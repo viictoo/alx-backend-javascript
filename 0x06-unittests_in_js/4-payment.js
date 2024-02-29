0e54a2ae17ffb22bd700688497b3d4eeab3a1649
Victor Langat <provicml@gmail.com>
2024-02-29 12:16:19 +0300

    stub expensive api call with sinon

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+// Stubs are similar to spies. Except that you can provide a
+// different implementation of the function you are wrapping
+
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: ${result}`);
+}
+  
+module.exports = sendPaymentRequestToApi;
