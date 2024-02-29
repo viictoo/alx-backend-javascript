688a9b31283229ba35875b5948152df6386f895f
Victor Langat <provicml@gmail.com>
2024-02-29 13:47:07 +0300

    test async using done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const { expect } = require('chai');
+
+const getPaymentTokenFromApi = require('./6-payment_token');
+
+describe('async test for function getPaymentTokenFromApi', () => {
+  it('should return an object when passed true', (done) => {
+    getPaymentTokenFromApi(true).then((res) => {
+      expect(res).to.include({ data: 'Successful response from the API' });
+      done();
+    }).catch((err) => done(err));
+  });
+});
