2e9d44da4d55ce61dd4df9b0a2c5e556a6e5bc5a
Victor Langat <provicml@gmail.com>
2024-02-29 13:08:33 +0300

    hook are called before each test

--- 0x06-unittests_in_js/5-payment.js
+++ 0x06-unittests_in_js/5-payment.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+// Hooks are useful functions that can be called before
+// execute one or all tests in a suite
+
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${result}`);
+}
+
+module.exports = sendPaymentRequestToApi;
