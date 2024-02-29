10be4f1b0038976e4e8b4c8fd539c18c05b28e48
Victor Langat <provicml@gmail.com>
2024-02-29 21:03:44 +0300

    fix: output

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -11,7 +11,7 @@ const sendPaymentRequestToApi = require('./3-payment.js');
 describe('function sendPaymentRequestToApcalls api', () => {
   const spy = sinon.spy(Utils, 'calculateNumber');
   it('should call the api function with passed args', () => {
-    sendPaymentRequestToApi(100.1, 20);
+    sendPaymentRequestToApi(100, 20);
     expect(spy.calledOnce).to.be.true;
     expect(spy.calledWith('SUM', 100, 20)).to.be.true;
     // always restore a spy after using it in a test,
